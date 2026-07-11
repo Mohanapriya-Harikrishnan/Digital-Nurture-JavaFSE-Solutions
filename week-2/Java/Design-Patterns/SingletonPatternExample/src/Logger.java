3d65b3c2bd080a992ca42ad54be0ddcc71ecf4b7
Mohanapriya <mohanapriyaharikrishnan13@gmail.com>
2026-07-11 14:24:08 +0530

    Completed Factory Method Pattern exercise

--- week-2/Java/Design-Patterns/SingletonPatternExample/src/Logger.java
+++ week-2/Java/Design-Patterns/SingletonPatternExample/src/Logger.java
@@ -1,17 +1,5 @@
 public class Logger {
 
-<<<<<<< HEAD
-    private static Logger instance;
-
-    private Logger() {
-        // Private constructor
-    }
-
-    public static Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
-        }
-=======
     // Single instance of Logger
     private static Logger instance;
 
@@ -22,12 +10,9 @@ public class Logger {
 
     // Method to get the single instance
     public static Logger getInstance() {
-
         if (instance == null) {
             instance = new Logger();
         }
-
->>>>>>> a4a7a8b (Completed Bootstrap Exercise 1.1 and Singleton Pattern Example)
         return instance;
     }
 }
